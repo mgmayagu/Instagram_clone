dea0b66820146be82358ba32f3d57c2d5bc72074
mgmayagu <mmayagu000@citymail.cuny.edu>
2021-03-20 18:58:55 -0500

    clone project

--- Instagram_clone/AppDelegate.swift
+++ Instagram_clone/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "NI0usmhYhM1whb7F0xXLtgKnN74s0dbKRO7oF6ps" // <- UPDATE
+                $0.clientKey = "mZ0RkVArHQnbfSgCorh6l0SBshFusBeN03O3KYHm" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
